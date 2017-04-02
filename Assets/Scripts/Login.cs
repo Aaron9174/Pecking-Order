3dfb3f196c8d03980608a6ed47efacc6de7efc3f
mpd18 <mpd18@knights.ucf.edu>
2017-04-01 23:32:01 -0400

    All the new stuff

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -10,57 +10,40 @@ public class Login : MonoBehaviour {
 	public GameObject password;
 	private string Username;
 	private string Password;
-	private String[] Lines;
-	private string DecryptedPass;
 
 	public void LoginButton(){
-		bool UN = false;
-		bool PW = false;
         string loginURL = "http://test1.xu4qu3w2zy.us-east-1.elasticbeanstalk.com/";
         WWWForm form = new WWWForm();
         form.AddField("login","true");
-        form.AddField("username", Username);
-        form.AddField("password", Password);
 
         /*WWW download = new WWW(loginURL, form);
         yield return download;*/
 
         if (Username != ""){
-
-			if(System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt")){
-				UN = true;
-				Lines = System.IO.File.ReadAllLines(@"E:/UnityTestFolder/"+Username+".txt");
-			} else {
-				Debug.LogWarning("Username Invaild");
-			}
-		} else {
+            Debug.Log(Username);
+            form.AddField("username", Username);
+        } else {
 			Debug.LogWarning("Username Field Empty");
 		}
-		if (Password != ""){
-			if (System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt")){
-				int i = 1;
-				foreach(char c in Lines[2]){
-					i++;
-					char Decrypted = (char)(c / i);
-					DecryptedPass += Decrypted.ToString();
-				}
-				if (Password == DecryptedPass){
-					PW = true;
-				} else {
-					Debug.LogWarning("Password Is invalid");
-				}
-			} else {
-				Debug.LogWarning("Password Is invalid");
-			}
-		} else {
+        if (Password != "") {
+            Debug.Log(Password);
+            form.AddField("password", Password);
+        }
+		 else {
 			Debug.LogWarning("Password Field Empty");
 		}
-		if (UN == true&&PW == true){
-			username.GetComponent<InputField>().text = "";
-			password.GetComponent<InputField>().text = "";	
-			print ("Login Sucessful");
-			SceneManager.LoadScene("Start Menu");
-		}
+        WWW w = new WWW(loginURL, form);
+        while (!w.isDone) { }
+        string result = w.text;
+        Debug.Log(result);
+        if (result=="true")
+        {
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            print("Login Sucessful");
+            //uncomment this after the php is debugged
+            //SceneManager.LoadScene("Main Menu");
+        }
 	}
 
     // Update is called once per frame
--- Assets/Scripts/Register.cs
+++ Assets/Scripts/Register.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System;
 using System.Text.RegularExpressions;
@@ -8,7 +9,10 @@ public class Register : MonoBehaviour {
 	public GameObject username;
 	public GameObject password;
 	public GameObject confPassword;
-	private string Username;
+    public GameObject usernameTaken;
+    public GameObject RegSuccess;
+    public GameObject RegFail;
+    private string Username;
 	private string Password;
 	private string ConfPassword;
 	private string form;
@@ -17,73 +21,71 @@ public class Register : MonoBehaviour {
 								   "1","2","3","4","5","6","7","8","9","0","_","-"};
 	
 	public void RegisterButton(){
-		bool UN = false;
-		bool EM = false;
-		bool PW = false;
 		bool CPW = false;
 
-        /*string loginURL = "http://test1.xu4qu3w2zy.us-east-1.elasticbeanstalk.com/";
+        string loginURL = "http://test1.xu4qu3w2zy.us-east-1.elasticbeanstalk.com/";
         WWWForm form = new WWWForm();
         form.AddField("register", "true");
-        form.AddField("username", Username);
-        form.AddField("password", Password);
-
-        /*WWW download = new WWW(loginURL, form);
-        yield return download;*/
 
         if (Username != "")
         {
-            //check if username is already taken here, if yes warning, if not continue on
-			if (!System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt"))
-            {
-				UN = true;
-			}
-            else
-            {
-				Debug.LogWarning("Username Taken");
-			}
-		}
+            Debug.Log(Username);
+            form.AddField("username", Username);
+        }
         else
         {
 			Debug.LogWarning("Username field Empty");
 		}
-		if (Password != ""){
-			if(Password.Length > 5){
-				PW = true;
-			} else {
-				Debug.LogWarning("Password Must Be atleast 6 Characters long");
-			}
-		} else {
+		if (Password != "")
+        {
+            Debug.Log(Password);
+            form.AddField("password", Password);
+        }
+        else
+        {
 			Debug.LogWarning("Password Field Empty");
 		}
 		if (ConfPassword != ""){
 			if (ConfPassword == Password){
 				CPW = true;
-			} else {
+                Debug.Log("the passwords match");
+            } else {
 				Debug.LogWarning("Passwords Don't Match");
 			}
 		} else {
 			Debug.LogWarning("Confirm Password Field Empty");
 		}
-		if (UN == true&&EM == true&&PW == true&&CPW == true){
-			bool Clear = true;
-			int i = 1;
-			foreach(char c in Password){
-				if (Clear){
-					Password = "";
-					Clear = false;
-				}
-				i++;
-				char Encrypted = (char)(c * i);
-				Password += Encrypted.ToString();
-			}
-            //once it passes all this stuff the users information needs to be enterd into the database through a php script
-			form = (Username+Environment.NewLine+Environment.NewLine+Password);
-			System.IO.File.WriteAllText(@"E:/UnityTestFolder/"+Username+".txt", form);
-			username.GetComponent<InputField>().text = "";
-			password.GetComponent<InputField>().text = "";
-			confPassword.GetComponent<InputField>().text = "";
-			print ("Registration Complete");
+		if (CPW == true){
+            //waiting for the thing to return 
+            WWW download = new WWW(loginURL, form);
+            while (!download.isDone) {}
+            string result = download.text;
+            Debug.Log(result);
+            if (result=="taken")
+            {
+                Debug.Log("username taken");
+                usernameTaken.SetActive(true);
+                username.GetComponent<InputField>().text = "";
+                password.GetComponent<InputField>().text = "";
+                confPassword.GetComponent<InputField>().text = "";
+            }
+            else if(result=="success")
+            {
+                Debug.Log("registerd");
+                RegSuccess.SetActive(true);
+                username.GetComponent<InputField>().text = "";
+                password.GetComponent<InputField>().text = "";
+                confPassword.GetComponent<InputField>().text = "";
+            }
+            else
+            {
+                Debug.Log("server error");
+                RegFail.SetActive(true);
+                username.GetComponent<InputField>().text = "";
+                password.GetComponent<InputField>().text = "";
+                confPassword.GetComponent<InputField>().text = "";
+            }
+			
 		}
 
 	}
@@ -107,5 +109,18 @@ public class Register : MonoBehaviour {
 		Password = password.GetComponent<InputField>().text;
 		ConfPassword = confPassword.GetComponent<InputField>().text;
 	}
+    /*IEnumerator WaitForRequest(WWW download)
+    {
+        yield return download;
+
+        if (download.error == null)
+        {
+            Debug.Log("WWW Ok!: " + download.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + download.error);
+        }
+    }*/
 
 }
