347318c4a2feaecfca1f3e9951c6e4c9f2a144fc
Aaron Hebson <Aaron9174@yahoo.com>
2017-04-02 23:59:39 -0400

    login system is fully functioning

--- Assets/Scripts/LogoutServer.cs
+++ Assets/Scripts/LogoutServer.cs
@@ -4,20 +4,24 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 using System.Text.RegularExpressions;
+
 public class LogoutServer : MonoBehaviour
 {
     public void logout()
     {
         string loginURL = "http://test1.xu4qu3w2zy.us-east-1.elasticbeanstalk.com/";
-        WWWForm form = new WWWForm(); form.AddField("logout", "true");
+        WWWForm form = new WWWForm();
+        form.AddField("logout", "true");
         string username = PlayerPrefs.GetString("username");
         form.AddField("username", username);
+        Debug.Log(username);
         WWW w = new WWW(loginURL, form);
-        while (!w.isDone) {}
+        while (!w.isDone) { }
         string result = w.text;
         if (result == "logout")
         {
             Debug.Log("LogOut was successful");
+            Debug.Log(result);
             SceneManager.LoadScene("Login Menu");
         }
         else
